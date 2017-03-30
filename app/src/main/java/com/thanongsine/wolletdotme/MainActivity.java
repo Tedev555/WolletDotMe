6ae6877eaaeb5ff2cc4819f7765c6f26847a3293
ted555 <thanong984@gmail.com>
2017-03-31 01:11:33 +0700

    design ui for login

--- app/src/main/java/com/thanongsine/wolletdotme/MainActivity.java
+++ app/src/main/java/com/thanongsine/wolletdotme/MainActivity.java
@@ -2,6 +2,10 @@ package com.thanongsine.wolletdotme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText usernameTxt = (EditText) findViewById(R.id.username_txt);
+        EditText passTxt = (EditText) findViewById(R.id.password_txt);
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
